fa25953e84be40b6fe1215821ce599f7e0c20ce6
radhagawande20011 <radhagawande20011@gmail.com>
2024-09-26 11:46:05 +0530

    Reactive organization form

--- src/app/demo/organization/add-organization/add-organization.component.ts
+++ src/app/demo/organization/add-organization/add-organization.component.ts
@@ -1,24 +1,79 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrganizationService } from '../service/organization.service';
 
 @Component({
   selector: 'app-add-organization',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './add-organization.component.html',
   styleUrl: './add-organization.component.scss'
 })
-export class AddOrganizationComponent {
+export class AddOrganizationComponent implements OnInit {
 
-  constructor(private route: Router){}
+  submitted: boolean = false;
+  orgForm!: FormGroup;
+  isEditMode: boolean = false;
+  loading: boolean = false;
 
-  navigateToBack(){
+  get fc() {
+    return this.orgForm.controls;
+  }
+
+  get fv() {
+    return this.orgForm.value;
+  }
+
+  constructor(
+    private route: Router,
+    private activate: ActivatedRoute,
+    private fb: FormBuilder,
+    private orgService: OrganizationService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  initializeForm() {
+    this.orgForm = this.fb.group({
+      Name: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.email, Validators.required]),
+      Domain: new FormControl('', [Validators.required]),
+      MobileNo: new FormControl('', [Validators.required]),
+      Address: new FormControl('', [Validators.required]),
+      VillageName: new FormControl('', [Validators.required]),
+      OrganizationCode: new FormControl('', [Validators.required]),
+    });
+  }
+  navigateToBack() {
     this.route.navigate(['/'])
 
   }
 
-  onSubmit(){
-    
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true;
+
+    if (this.orgForm.invalid) {
+      this.loading = false;
+      return;
+    }
+
+    const organization = this.orgForm.value;
+
+    // if (!this.id) {
+    //   this.userService.addUser(user).subscribe({
+    //     next: (res: any) => {
+    //       this.loading = false;
+    //     },
+    //     error: (error) => {
+    //       this.loading = false;
+    //       setTimeout(() => this.showErrorAlert = false, 3000);
+    //     }
+    //   });
+    // } 
+
   }
 
 }
--- src/app/demo/template/template-list/template-list.component.ts
+++ src/app/demo/template/template-list/template-list.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { SearchComponent } from 'src/app/theme/shared/components/search/search.component';
 
 @Component({
   selector: 'app-template-list',
   standalone: true,
-  imports: [],
+  imports: [NgxSkeletonLoaderModule, CommonModule, SearchComponent],
   templateUrl: './template-list.component.html',
   styleUrl: './template-list.component.scss'
 })
 export class TemplateListComponent {
+
+  isLoading: boolean = false;
+
+  constructor(private route: Router){
+
+  }
+
+  navigateToAddEmail(){
+    this.route.navigate(['template/add'])
+  }
 
 }
